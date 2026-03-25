71b412d13d79496ba45d1deccfc62ba36259ff0f
Nhat Minh <nguyenhuunhatminh426@gmail.com>
2026-03-25 22:53:43 +0700

    add library jpa

---
+++ practice34/src/main/java/com/cybersoft/practice34/entity/CustomerEntity.java
@@ -0,0 +1,11 @@
+package com.cybersoft.practice34.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class CustomerEntity {
+}
